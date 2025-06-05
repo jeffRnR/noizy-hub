b8efd1daaeb3bc686ff51c538a9ed07c021c2126
jeffRnR <jeffmunyigi@gmail.com>
2025-06-05 10:55:33 +0300

    wsb

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -71,7 +71,7 @@ function EventPage() {
                     </p>
                   </div>
 
-                  <div className="bg-gray-50 p-4 rounded-md border border-gray-50">
+                  <div className="bg-gray-50 p-4 rounded-md border border-gray-100">
                     <div className="flex items-center text-gray-600 mb-1">
                         <MapPin className="w-5 h-5 mr-2 text-[#7adb78]"/>
                         <span>Location</span>
